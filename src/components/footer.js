a8a727758718d05620ca697ba548abd410c8702d
Carolina Gomez Gilabert <carolgilabert@gmail.com>
2019-03-23 10:48:21 +0000

    Adding a (very simple for now) footer

---
+++ src/components/footer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Footer = () => (
+    <footer style={{
+        textAlign: 'center',
+        fontSize: '1.2em'
+    }}>
+        Built with 
+        <span role="img" aria-label="yellow heart emoji"> 💛 </span> 
+        by <a target="_blank" rel="noopener noreferrer" href="https://carolgilabert.me">Carol</a>
+    </footer>
+);
+
+export default Footer;
